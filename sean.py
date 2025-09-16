69ca126c8ad8a725a51af9e9eed92aea24a4e08b
Seannnn1 <sean.arthur@ashesi.edu.gh>
2025-09-16 23:50:59 +0000

    Create sean.py

---
+++ sean.py
@@ -0,0 +1,12 @@
+def main():
+    # Something interesting about me
+    info = """
+    Hello! 
+    I am Sean, a university student who loves to do so many things.
+    I love chatting about anything from tech and science to movies and hobbies.
+    """
+
+    print(info)
+
+if __name__ == "__main__":
+    main()
